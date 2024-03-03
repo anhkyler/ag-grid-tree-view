2fcccd55de46b1cd56c3e313125c935e88ec85b5
Benj So <benjso@Benjs-MBP.lan>
2024-03-02 22:08:03 -0500

    upgrade dependencies

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AgGridAngular } from 'ag-grid-angular'; // AG Grid Component
+import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ];
+
+  ngOnInit() {
+    // fetch('https://www.ag-grid.com/example-assets/row-data.json')
+    //   .then(result => result.json())
+    //   .then(rowData => this.rowData = rowData);
+  }
   constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
