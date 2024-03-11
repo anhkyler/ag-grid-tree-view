964b1a3bac177919b66104a46a463f71a03d0e3e
Benj So <benjso@Benjs-MBP.lan>
2024-03-11 17:07:10 -0400

    commit account change

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular'; // AG Grid Component
-import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+import { ColDef, GetDataPath, GridApi, GridReadyEvent } from 'ag-grid-community'; // Column Definition Type Interface
+import { getData } from './data';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -8,27 +9,58 @@ import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
 })
 export class AccountComponent implements OnInit {
 
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ];
+  // rowData = [
+  //   { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  //   { make: "Ford", model: "F-Series", price: 33850, electric: false },
+  //   { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  // ];
 
-  // Column Definitions: Defines the columns to be displayed.
-  colDefs: ColDef[] = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
-  ];
+  // // Column Definitions: Defines the columns to be displayed.
+  // colDefs: ColDef[] = [
+  //   { field: "make" },
+  //   { field: "model" },
+  //   { field: "price" },
+  //   { field: "electric" }
+  // ];
 
   ngOnInit() {
-    // fetch('https://www.ag-grid.com/example-assets/row-data.json')
-    //   .then(result => result.json())
-    //   .then(rowData => this.rowData = rowData);
-  }
+    }
   constructor() { }
+  
+  private gridApi!: GridApi;
 
+  public columnDefs: ColDef[] = [
+    // we're using the auto group column by default!
+    { field: 'jobTitle' },
+    { field: 'employmentType' },
+  ];
+  public defaultColDef: ColDef = {
+    flex: 1,
+  };
+  public autoGroupColumnDef: ColDef = {
+    headerName: 'Organisation Hierarchy',
+    minWidth: 300,
+    cellRendererParams: {
+      suppressCount: true,
+    },
+  };
+  public rowData: any[] | null = getData();
+  public groupDefaultExpanded = -1;
+  public getDataPath: GetDataPath = (data: any) => {
+    return data.orgHierarchy;
+  };
+  public themeClass: string =
+    "ag-theme-quartz";
 
+  onFilterTextBoxChanged() {
+    this.gridApi.setGridOption(
+      'quickFilterText',
+      (document.getElementById('filter-text-box') as any).value
+    );
+  }
+
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi = params.api;
+  }
 
 }
